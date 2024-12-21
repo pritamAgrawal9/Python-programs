dfdf040595f5bf8c6a7feb22bc2e5d0e81f7ab40
Prashant Agrawal <pritamtg99999@gmail.com>
2024-12-21 14:55:47 +0530

    play

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from turtle import Turtle, Screen
 from box import Box
 from ball import Ball
+from scoreboard import Scoreboard
 import time
+
 
 screen = Screen()
 screen.bgcolor("black")
@@ -13,15 +15,16 @@ screen.tracer(0)
 r_box = Box((350, 0))
 ball = Ball()
 l_box = Box((-350, 0))
+scoreboard = Scoreboard()
 
 screen.onkey(r_box.go_up,"Up")
 screen.onkey(l_box.go_up,"w")
 screen.onkey(r_box.go_down,"Down")
 screen.onkey(l_box.go_down,"s")
-
+t_ime = 0.1
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(t_ime)
     ball.move()
     screen.update()
 
@@ -32,12 +35,21 @@ while game_is_on:
     # Detect collision with paddle
     if ball.distance(r_box) < 50 and ball.xcor() > 320 or ball.distance(l_box) < 50 and ball.xcor() < -320:
         ball.bounce_x()
+        t_ime -= 0.005
+
+
     if ball.xcor() > 370 :
+        t_ime = 0.1
+        scoreboard.l_point()
         ball.reset_position()
         ball.bounce_x()
+
     if ball.xcor() < -370:
+        t_ime = 0.1
+        scoreboard.r_point()
         ball.reset_position()
         ball.bounce_x()
+
 
 
 
